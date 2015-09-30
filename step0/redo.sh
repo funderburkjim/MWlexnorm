8d4dbc60508e5d6170477116954246c8317ea1d6
funderburkjim <funderburkjim@gmail.com>
2015-09-30 17:11:02 -0400

    Revise to validate lexicalgrammar.xml. change location of mw.xml input

--- step0/redo.sh
+++ step0/redo.sh
@@ -1,2 +1,4 @@
-echo "remake lexicalgrammar from mw.xml and DualPlural"
-python26 lexicalgrammar_dp.py ../../../pywork/mw.xml DualPlural.txt lexicalgrammar.xml
+echo "remake lexicalgrammar from mw.xml and DualPlural, participle"
+python lexicalgrammar_dp.py ../../mwxml/mw.xml DualPlural.txt participle.txt lexicalgrammar.xml
+echo "validating lexicalgrammar.xml with lexicalgrammar.dtd"
+python validate.py lexicalgrammar.xml lexicalgrammar.dtd
