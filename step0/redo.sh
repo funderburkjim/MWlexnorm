6184b5143e520e0c7ccd2a095a529dc54fe49a5a
funderburkjim <funderburkjim@gmail.com>
2015-12-08 16:08:33 -0500

    update dualplural.txt, and redo_all.sh

--- step0/redo.sh
+++ step0/redo.sh
@@ -1,3 +1,7 @@
+echo "mw.xml is in a directory 'mwxml' ABOVE MWlexnorm."
+echo "This directory may be downloaded (in compressed form) by"
+echo "curl -o mwxml.zip http://www.sanskrit-lexicon.uni-koeln.de/scans/MWScan/2014/downloads/mwxml.zip"
+echo 
 echo "remake lexicalgrammar from mw.xml and DualPlural, participle"
 python lexicalgrammar_dp.py ../../mwxml/mw.xml DualPlural.txt participle.txt lexicalgrammar.xml
 echo "validating lexicalgrammar.xml with lexicalgrammar.dtd"
